bdd62bef24627d278fbfe392b729768035fa7341
linqinong <linqinong@163.com>
2015-02-13 22:35:47 +0800

    .net 三层代码生成
    根据数据库表自动生成代码。

---
+++ Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+    Public s As Module1.Server
+
+    Structure Server
+        Dim ServerName As String
+        Dim Uid As String
+        Dim pwd As String
+        Dim DbName As String
+    End Structure
+End Module
